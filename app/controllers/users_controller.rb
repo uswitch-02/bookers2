43feec03662cae8e8dd3240b2647a62dd26c37a4
uswith <124918064+uswitch-02@users.noreply.github.com>
2023-03-27 15:53:45 +0000

    [update]bookers2

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,5 +8,12 @@ class UsersController < ApplicationController
   end
 
   def edit
+    @user = User.find(params:[id])
+  end
+  
+  def updated
+    @user = User.find(params:[id])
+    @user.update(user_params)
+    redirect_to user_path(user.id)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "homes#top"
   resources :books, only: [:index, :show, :create, :destroy]
-  resources :users, only: [:show, :index, :edit]
+  resources :users, only: [:show, :index, :edit, :update]
 
 
   get "/homes/about" => "homes#about" , as: "about"
