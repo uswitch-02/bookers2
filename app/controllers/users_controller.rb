00f4b25ab9dc3993158afd28a6ac4dfd92f0365d
uswith <124918064+uswitch-02@users.noreply.github.com>
2023-03-27 13:28:29 +0000

    [update]bookers2

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,11 @@
+class UsersController < ApplicationController
+  def show
+    @user = 
+  end
+
+  def index
+  end
+
+  def edit
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
+  
   devise_for :users
   root to: "homes#top"
   resources :books, only: [:index, :show, :create, :destroy]
+  resources :users, only: [:show, :index, :edit]
 
 
   get "/homes/about" => "homes#about" , as: "about"
