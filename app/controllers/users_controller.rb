d029843df9ec88b9e9e9757d956c6f70257a5b09
uswith <124918064+uswitch-02@users.noreply.github.com>
2023-03-30 17:55:37 +0000

    [update]bookers2

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,9 +12,15 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
-  def updated
+  def update
     @user = User.find(params[:id])
     @user.update(user_params)
-    redirect_to user_path(user.id)
+    redirect_to user_path(@user.id)
+  end
+  
+  private
+  
+  def user_params
+    params.require(:user).permit(:name, :profile_image, :introduction )
   end
 end
